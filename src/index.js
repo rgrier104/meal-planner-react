54ee582302cf336dd14bf25b2f9dbd4d74a5a7ea
rgrier104 <rgrier104@gmail.com>
2020-03-11 14:04:50 -1000

    add reducer file and import to app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import reducer from './reducers/reducer';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
