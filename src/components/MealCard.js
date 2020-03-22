f0d378c4301936cd70f78b551ec74740b6bb1d8d
rgrier104 <rgrier104@gmail.com>
2020-03-21 14:31:34 -1000

    add MealCard component that will render a form to select meal

---
+++ src/components/MealCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealCard = ({ meal }) => {
+
+    return (
+        <div>
+            Display Modal to select {meal};
+        </div>
+    )
+}
+
+export default MealCard;
