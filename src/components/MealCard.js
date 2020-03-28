92f9a1a78e7e03a3b86468f14ec6b8510629099a
rgrier104 <rgrier104@gmail.com>
2020-03-28 13:00:52 -1000

    add modal component and state to control when modal is displayed

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
+import Modal from './Modal';
 
 const MealCard = ({ meal }) => {
 
+    const [show, setShow] = useState(false);
+    const openModal = () => setShow(true);
+    const closeModal = () => setShow(false);
+
     return (
         <div>
-            Display Modal to select {meal};
+            <button onClick={openModal}>Select {meal}</button>
+            <Modal show={show} closeModal={closeModal} meal={meal} />
         </div>
     )
 }
