e6301fdf840d900a6be50d4b51ab3879b2d5a3ad
rgrier104 <rgrier104@gmail.com>
2020-04-05 16:15:45 -1000

    add meal form with all recipes retreived from the Redux store

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Modal = ({ show, closeModal, meal }) => {
+
+    const recipes = useSelector(state => state.recipesReducer.recipes)
 
     return (
         <div className={show ? "overlay" : "hide"}>
             <div className={show ? "modal" : "hide"}>
                 <button onClick={closeModal}>X</button>
-                Select {meal} form
+                Select {meal}:
+                <form>
+                    <select>
+                        {recipes.map(recipe => <option>{recipe.name}</option>)}
+                    </select>
+                    <input type="submit" value="Add Meal" />
+                </form>
             </div>
         </div>
         
