48b78d6ccba58ea09fe9987a2ce96d3fa398fbf0
rgrier104 <rgrier104@gmail.com>
2020-04-06 16:49:56 -1000

    add router links to home page for app navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Meal Planner</h1>
+            <div className="routerLinksContainer">
+                <Link to="/meal_plans/new" className="routerLinks">Create a New Meal Plan</Link>
+                <Link to="/meal_plans" className="routerLinks">View Old Meal Plans</Link>
+                <Link to="/recipes" className="routerLinks">View Recipes</Link>
+            </div>
         </div>
     )
 }
