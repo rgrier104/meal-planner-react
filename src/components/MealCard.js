57b885ed701c5426ba37405c1a40b6c9f32d5fcd
rgrier104 <rgrier104@gmail.com>
2020-04-10 16:23:39 -1000

    add recipe to state using useState hook and add logic to show recipe if selected

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useState } from 'react';
 import Modal from './Modal';
 
@@ -8,10 +8,19 @@ const MealCard = ({ meal }) => {
     const openModal = () => setShow(true);
     const closeModal = () => setShow(false);
 
+    // Add recipe to meal card - probably change to useSelector to connect with Redux store after backend is connected
+    const [recipe, setRecipe] = useState("")
+
     return (
         <div>
-            <button onClick={openModal}>Select {meal}</button>
-            <Modal show={show} closeModal={closeModal} meal={meal} />
+            {recipe ? (
+                <a href="" target="_blank">{recipe.name}</a>
+            ) : (
+                <Fragment>
+                    <button onClick={openModal}>Select {meal}</button>
+                    <Modal show={show} closeModal={closeModal} meal={meal} setRecipe={setRecipe} />
+                </Fragment>
+            )}
         </div>
     )
 }
