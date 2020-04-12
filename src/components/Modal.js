4f5e5db41ddf4aae3a52d13918c621eec66ae28e
rgrier104 <rgrier104@gmail.com>
2020-04-11 14:46:27 -1000

    add handleSubmit function to add selected recipe to meal card state

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-const Modal = ({ show, closeModal, meal }) => {
+const Modal = ({ show, closeModal, meal, handleChange }) => {
     
     
     const recipes = useSelector(state => state.recipesReducer.recipes.filter(recipe => {
@@ -11,15 +11,22 @@ const Modal = ({ show, closeModal, meal }) => {
             return recipe.meal_type === meal
         } 
     }))
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        let recipe = recipes.filter(recipe => recipe.id === parseInt(e.target.recipeName.value))
+        handleChange(recipe[0].name, recipe[0].url)
+        // update to make cleaner
+    }
     
     return (
         <div className={show ? "overlay" : "hide"}>
             <div className={show ? "modal" : "hide"}>
                 <button onClick={closeModal}>X</button>
                 Select {meal}:
-                <form>
-                    <select>
-                        {recipes.map(recipe => <option key={recipe.id}>{recipe.name}</option>)}
+                <form onSubmit={handleSubmit}>
+                    <select name="recipeName" >
+                        {recipes.map(recipe => <option key={recipe.id} value={recipe.id}>{recipe.name}</option>)}
                     </select>
                     <input type="submit" value="Add Meal" />
                 </form>
